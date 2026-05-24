70842d7d237625c8488e98af9f9f9f7d56fb55cf
LOEA <lea.hobein@gmail.com>
2026-05-24 16:39:04 +0200

    ADD Font, Timer

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,38 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float time = 10;
+    public TMP_Text timerText;
+    public bool timeUp = false;
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timerText.text = Mathf.Floor(time).ToString();
+
+        if (time < 11)
+        {
+            timerText.color = Color.indianRed;
+        }
+
+        if (time <= 1)
+        {
+            Pause();
+            timeUp = true;
+            Debug.Log("game over");
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+    }
+}
