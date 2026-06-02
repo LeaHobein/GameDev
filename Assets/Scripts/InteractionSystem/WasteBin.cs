83917ed9d9f95fa9c223d0d8186b00068a5187e3
Krys5 <krystyna.okwieka@stud.hshl.de>
2026-06-02 14:38:25 +0200

    ADD Waste Bin, Delete Item

---
+++ Assets/Scripts/InteractionSystem/WasteBin.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class WasteBin : MonoBehaviour, IInteractable
+{
+    //public GameObject other;
+   /*void Update()
+    {
+        if(Keyboard.current.xKey.wasPressedThisFrame)
+        {
+        Destroy(other);
+        Debug.Log("Item deleted");
+        }
+
+    }*/
+
+
+    void DestroyItem(GameObject player)
+    {
+        player.transform.Find("hold1").GetComponent<MeshRenderer>().enabled = false;
+    }
+
+
+    public void Interact(GameObject player)
+    {
+       DestroyItem(player);
+    }
+}
