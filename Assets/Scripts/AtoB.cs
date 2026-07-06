bd60a5b049796749fd27142119694e7de9e4c06e
LOEA <lea.hobein@gmail.com>
2026-07-06 15:03:15 +0200

    MOD unique Names, Code cleanup

--- Assets/Scripts/AtoB.cs
+++ Assets/Scripts/AtoB.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Xml.Linq;
 using UnityEngine;
 
 public class AtoB : MonoBehaviour
--- Assets/Scripts/InteractionSystem/DeliverySpot.cs
+++ Assets/Scripts/InteractionSystem/DeliverySpot.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using System.Collections;
 
 public class DeliverySpot : MonoBehaviour, IInteractable
